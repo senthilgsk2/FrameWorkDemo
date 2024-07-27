5794759c97841a8673415da1893fe9f0d6af011a
senthilgsk2 <mgsenthilmgs@gmail.com>
2024-07-27 10:04:43 +0530

    frame work commit

--- src/test/java/demo/steps/Steps.java
+++ src/test/java/demo/steps/Steps.java
@@ -64,5 +64,6 @@ public class Steps extends BaseClass{
 	@Then("test outline then")
 	public void test_outline_then() {
 	    System.out.println("test git pull");
+	    System.out.println("hey iam pushing this please wait");
 	}
 }
