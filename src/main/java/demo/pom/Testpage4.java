e5d033745d21772cf038b6a9cb9a1103564125b2
senthilgsk2 <mgsenthilmgs@gmail.com>
2024-07-27 10:56:57 +0530

    frame work commit branch 1

--- src/main/java/demo/pom/Testpage4.java
+++ src/main/java/demo/pom/Testpage4.java
@@ -4,5 +4,6 @@ public class Testpage4 {
 	public static void testbranch1() {
 		System.out.println("test page 4");
 		System.out.println("test page 4 2");
+		System.out.println("test page 4 3");
 	}
 }
